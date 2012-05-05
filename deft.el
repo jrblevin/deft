jrblevin/deft

commit df15ec1c7d3fe9667277511c22fba80b7fb13967

Make deft-filter (C-c C-l) respect incremental vs. regexp search option

deft.el
@@ -730,7 +730,10 @@ If the point is not on a file widget, do nothing."
 (defun deft-filter (str)
   "Set the filter string to STR and update the file browser."
   (interactive "sFilter: ")
-  (cond ((called-interactively-p 'any) (setq deft-filter-regexp (list str)))
+  (cond ((called-interactively-p 'any)
+         (if deft-incremental-search
+             (setq deft-filter-regexp (reverse (split-string str " ")))
+           (setq deft-filter-regexp (list str))))
 	((= (length str) 0) (setq deft-filter-regexp (cdr deft-filter-regexp)))
 	(t (setcar deft-filter-regexp str)))
   (deft-filter-update)

commit 0b664ca079a0cd5e8322ed5351f01403bffcf03d

Fix deft-filter in incremental search mode
Broken by df15ec1

deft.el
@@ -749,15 +749,21 @@ If the point is not on a file widget, do nothing."
   (message "Filter cleared."))
 
 (defun deft-filter (str)
-  "Set the filter string to STR and update the file browser."
+  "Update the filter string with STR and update the file browser.
+In incremental search mode, STR will be added to the list of
+filter strings.  If STR has zero length, one element is removed
+from the list.  In regex search mode, the current filter string
+will be replaced with STR."
   (interactive "sFilter: ")
-  (cond
-   ((= (length str) 0)
-    (setq deft-filter-regexp nil))
-   (t
-    (if deft-incremental-search
-        (setq deft-filter-regexp (reverse (split-string str " ")))
-      (setq deft-filter-regexp (list str)))))
+  (if deft-incremental-search
+      (if (called-interactively-p 'any)
+          (if (= (length str) 0)
+              (setq deft-filter-regexp nil)
+            (setq deft-filter-regexp (reverse (split-string str " "))))
+        (if (= (length str) 0)
+            (setq deft-filter-regexp (cdr deft-filter-regexp))
+          (setcar deft-filter-regexp str)))
+    (setq deft-filter-regexp (list str)))
   (deft-filter-update)
   (deft-refresh-browser))
 