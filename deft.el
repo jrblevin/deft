jrblevin/deft

commit c203ee3912be0ccfbc4f4ff9cd78a69dfab89f41

Refactor and simplify new file functions

deft.el
@@ -655,6 +655,19 @@ Call this function after any actions which update the filter and file list."
 
 ;; File list file management actions
 
+(defun deft-unused-filename ()
+  (let (fmt counter slug dir file)
+    (setq fmt (concat "deft-%d"))
+    (setq dir (file-name-as-directory deft-directory))
+    (setq counter 0)
+    (setq slug (format fmt counter))
+    (setq file (concat dir slug "." deft-extension))
+    (while (or (file-exists-p file) (get-file-buffer file))
+      (setq counter (1+ counter))
+      (setq slug (format fmt counter))
+      (setq file (concat dir slug "." deft-extension)))
+    slug))
+
 (defun deft-open-file (file)
   "Open FILE in a new buffer and setting its mode."
   (prog1 (find-file file)
@@ -680,9 +693,14 @@ use it as the title."
                      file "." deft-extension))
   (if (file-exists-p file)
       (message (concat "Aborting, file already exists: " file))
+    ;; Insert the contents of the filter string in the file.
     (when (and deft-filter-regexp (not deft-use-filename-as-title))
-      (write-region (deft-whole-filter-regexp) nil file nil))
-    (deft-open-file file)))
+      (write-region (concat (deft-whole-filter-regexp) "\n\n") nil file nil))
+    (deft-cache-update-file file)
+    (deft-refresh-filter)
+    (deft-open-file file)
+    (with-current-buffer (get-file-buffer file)
+      (goto-char (point-max)))))
 
 ;;;###autoload
 (defun deft-new-file ()
@@ -693,24 +711,15 @@ filter string is non-nil and title is not from filename, use it
 as the title."
   (interactive)
   (let (file)
-    (if (and deft-use-filename-as-title deft-filter-regexp)
+    (if (and deft-filter-regexp deft-use-filename-as-title)
+        ;; If the filter string is non-emtpy and titles are taken from
+        ;; filenames is set, construct filename from filter string.
 	(setq file (concat (file-name-as-directory deft-directory)
                            (deft-whole-filter-regexp) "." deft-extension))
-      (let (fmt counter temp-buffer)
-	(setq counter 0)
-	(setq fmt (concat "deft-%d." deft-extension))
-	(setq file (concat (file-name-as-directory deft-directory)
-                           (format fmt counter)))
-	(while (or (file-exists-p file)
-		   (get-file-buffer file))
-	  (setq counter (1+ counter))
-	  (setq file (concat (file-name-as-directory deft-directory)
-                             (format fmt counter))))
-	(when deft-filter-regexp
-	  (write-region (concat (deft-whole-filter-regexp) "\n\n") nil file nil))))
-    (deft-open-file file)
-    (with-current-buffer (get-file-buffer file)
-      (goto-char (point-max)))))
+      ;; If the filter string is empty, or titles are taken from file
+      ;; contents, then use an automatically generated unique filename.
+      (setq file (deft-unused-filename)))
+    (deft-new-file-named file)))
 
 (defun deft-delete-file ()
   "Delete the file represented by the widget at the point.

commit ffc1afe797459a41a472ecda125a2532850e165c

Use slug instead of full path when calling deft-new-file-named
Fixes a bug introduced in commit c203ee3912be0ccfbc4f4ff9cd78a69dfab89f41,
where creating a file when the filter string and deft-use-filename-as-title
are both non-nil results in an invalid path.

deft.el
@@ -764,6 +764,7 @@ OTHER and SWITCH are both non-nil, switch to the other window."
 
 (defun deft-new-file-named (file)
   "Create a new file named FILE (or interactively prompt for a filename).
+FILE must be a short name, without a path or a file extension.
 If the filter string is non-nil and title is not from file name,
 use it as the title."
   (interactive "sNew filename (without extension): ")
@@ -792,8 +793,7 @@ as the title."
     (if (and deft-filter-regexp deft-use-filename-as-title)
         ;; If the filter string is non-emtpy and titles are taken from
         ;; filenames is set, construct filename from filter string.
-	(setq file (concat (file-name-as-directory deft-directory)
-                           (deft-whole-filter-regexp) "." deft-extension))
+	(setq file (deft-whole-filter-regexp))
       ;; If the filter string is empty, or titles are taken from file
       ;; contents, then use an automatically generated unique filename.
       (setq file (deft-unused-filename)))