92f5f24efff69dbabeb460343c47125126a370d8
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2015-10-29 16:04:24 +0100

    Fixed line width wrap for me.

--- deft.el
+++ deft.el
@@ -991,7 +991,7 @@ call the original string-width otherwise"
            (mtime (when deft-time-format
                     (format-time-string deft-time-format (deft-file-mtime file))))
            (mtime-width (deft-string-width mtime))
-           (line-width (- deft-window-width mtime-width))
+           (line-width (- deft-window-width mtime-width 1))
            (title-width (min line-width (deft-string-width title)))
            (summary-width (min (deft-string-width summary)
                                (- line-width
